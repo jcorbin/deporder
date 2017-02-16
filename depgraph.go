719752622cd73a9e066ac45eb03c1cda8b82bc19
Joshua T Corbin <jcorbin@wunjo.org>
2017-02-16 09:17:07 -0800

    hashDepGraph.add: de-morgan

--- depgraph.go
+++ depgraph.go
@@ -87,7 +87,7 @@ func (t *hashDepGraph) add(name string, ds ...dep) {
 	if len(ds) == 0 {
 		_, inG := t.g[n]
 		_, inH := t.h[n]
-		if !(inG || inH) {
+		if !inG && !inH {
 			t.f[n] = struct{}{}
 		}
 		return
